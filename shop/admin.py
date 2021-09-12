9250d68c764a0ca289e66b7686194a35ada07cc0
chiahong <chiahong@pop-os.localdomain>
2021-09-12 14:42:27 +0800

    update admins

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
 from .models import Category, Product
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug',
+    )
+    prepopulated_fields = {'slug': ('name', )}
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'price',
+        'stock',
+        'available',
+        'createdAt',
+        'updatedAt',
+    )
+    list_editable = (
+        'price',
+        'stock',
+        'available',
+    )
+    prepopulated_fields = {'slug': ('name', )}
+    list_per_page = 20
+
+
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- shop/models.py
+++ shop/models.py
@@ -10,10 +10,10 @@ class Category(models.Model):
     class Meta:
         ordering = ('name', )
         verbose_name = 'category'
-        verbose_name_plural = 'categoriees'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
-        return {self.name}
+        return self.name
 
 
 class Product(models.Model):
